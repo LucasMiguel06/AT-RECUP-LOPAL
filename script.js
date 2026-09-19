885670fada3dbf68b7c9651c61dd6185fbd52949
Aluno <lucasm.fatinatti@gmail.com>
2026-09-19 09:53:09 -0300

    Fiz a soma dos numeros inteiros de 1 até 100

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-for (let i = 1; i <= 50; i++) {
-if (i % 2 === 0) {
-  console.log(i);
-    }
+let somaInteiro = 0;
+
+for (let i = 1; i <= 100; i++) {
+    somaInteiro += i;
 }
+console.log("A soma dos numeros inteiros de 1 a 100 é igual a:", somaInteiro);
