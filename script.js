a6c7062ce20c75c686755b2ab5f8e7b8afb61c60
Aluno <lucasm.fatinatti@gmail.com>
2026-09-19 10:40:56 -0300

    Programa q simula operações em umca conta bancaria

--- script.js
+++ script.js
@@ -1,6 +1,39 @@
-let somaInteiro = 0;
+let saldoInicial = 1000;
+let escolha;
 
-for (let i = 1; i <= 100; i++) {
-    somaInteiro += i;
-}
-console.log("A soma dos numeros inteiros de 1 a 100 é igual a:", somaInteiro);
+do {
+    console.log("Conta bancaria");
+    console.log("1 - depositar");
+    console.log("2 - sacar");
+    console.log("3 - finalizar");
+
+    escolha = prompt("escolha uma opcao disponivel");
+
+    if (escolha == "1") {
+
+        let quantia = Number(prompt("escreva uma quantia para depositar"));
+
+        saldoInicial += quantia;
+
+        console.log("deposito pronto");
+        alert("saldo disponivel: R$ " + saldoInicial.toFixed(2));
+    } else if (escolha == "2") {
+
+        let quantia = Number(prompt("digite um valor para sacar"));
+
+        if (quantia <= saldoInicial) {
+            saldoInicial -= quantia;
+            alert("saque realizado");
+        } else {
+            alert("saldo insuficiente");
+        }
+
+        alert("saldo atual: R$ " + saldoInicial.toFixed(2));
+    } else if (escolha == "3") {
+        alert("programa encerrado");
+    } else {
+        alert("opcao indisponivel");
+    }
+} while (escolha != "3");
+
+''
