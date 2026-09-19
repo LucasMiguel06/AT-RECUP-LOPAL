ac0744802ac9801ba9b2dd5a07dfcf09dedcd94b
Aluno <lucasm.fatinatti@gmail.com>
2026-09-19 10:49:26 -0300

    Desenvolvi um programa que simula operações realizadas em uma conta bancaria

--- script.js
+++ script.js
@@ -1,5 +1,39 @@
-for (let i = 1; i <= 50; i++) {
-if (i % 2 === 0) {
-  console.log(i);
+let saldoInicial = 1000;
+let escolha;
+
+do {
+    console.log("Conta bancaria");
+    console.log("1 - depositar");
+    console.log("2 - sacar");
+    console.log("3 - finalizar");
+
+    escolha = prompt("escolha uma opcao disponivel");
+
+    if (escolha == "1") {
+
+        let quantia = Number(prompt("escreva uma quantia para depositar"));
+
+        saldoInicial += quantia;
+
+        console.log("deposito pronto");
+        alert("saldo disponivel: R$ " + saldoInicial.toFixed(2));
+    } else if (escolha == "2") {
+
+        let quantia = Number(prompt("digite um valor para sacar"));
+
+        if (quantia <= saldoInicial) {
+            saldoInicial -= quantia;
+            alert("saque realizado");
+        } else {
+            alert("saldo insuficiente");
+        }
+
+        alert("saldo atual: R$ " + saldoInicial.toFixed(2));
+    } else if (escolha == "3") {
+        alert("programa encerrado");
+    } else {
+        alert("opcao indisponivel");
     }
-}
+} while (escolha != "3");
+
+
